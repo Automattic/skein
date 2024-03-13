2c2e39016881d4e757991afcff9ba5f4ea3118d1
Joao Amaral <7281460+joaopamaral@users.noreply.github.com>
2024-03-13 12:13:46 -0300

    revert log

--- java/src/main/java/com/anaconda/skein/Driver.java
+++ java/src/main/java/com/anaconda/skein/Driver.java
@@ -441,9 +441,6 @@ public class Driver {
       appContext.setMaxAppAttempts(spec.getMaxAttempts());
       appContext.setNodeLabelExpression(Strings.emptyToNull(spec.getNodeLabel()));
       appContext.setApplicationTags(spec.getTags());
-      if (env.get("YARN_LOG_AGGREGATION_EXCLUDE_PATTERN") != null) {
-        LOG.info("YARN_LOG_AGGREGATION_EXCLUDE_PATTERN: {}", env.get("YARN_LOG_AGGREGATION_EXCLUDE_PATTERN"));
-      }
       appContext.setLogAggregationContext(
         LogAggregationContext.newInstance(
             env.get("YARN_LOG_AGGREGATION_INCLUDE_PATTERN"),
