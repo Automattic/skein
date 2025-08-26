5e2552c27fe5ab341195d6021b3e797c763b0098
Igor Khrol <khroliz@gmail.com>
2025-08-26 19:47:42 +0300

    Python 3.12 - versioneer

--- skein/__init__.py
+++ skein/__init__.py
@@ -6,6 +6,5 @@ from .exceptions import (SkeinError, ConnectionError, DriverNotRunningError,
 from .model import (Security, ApplicationSpec, Service, File, Resources,
                     FileType, FileVisibility, ACLs, Master, LogLevel)
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
